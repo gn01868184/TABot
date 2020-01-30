c56bb4a2495148fc8d287ed8b42a7c117fb649ce
gn01868184 <gn01868184@yahoo.com.tw>
2020-01-30 16:51:11 +0800

    更新意圖功能完成

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const functions = require('firebase-functions');
 //const http = require('http');
 //const neo4j = require('neo4j-driver');
 //const axios = require('axios');
-const intentsCreate = require('./intentsCreate.js');
+const intentsUpdate = require('./intentsUpdate.js');
 const sheetsGet = require('./sheetsGet.js');
 const sheetsUpdate = require('./sheetsUpdate.js');
 const entitiesUpdate = require('./entitiesUpdate.js');
@@ -18,8 +18,8 @@ const app = dialogflow({
 });
 
 app.intent('更新關鍵字實體', (conv) => {
-  return sheetsGet().then((sheets) => {
-    //console.log(sheets[1][1].split(", "));
+  const SPREADSHEET_ID = '1UIEQekmLryJJSolpJNc15qar1fp4q-txiQgw1X-leLk';
+  return sheetsGet(SPREADSHEET_ID).then((sheets) => {
     let entityList = [];
     for(let i=1;i<sheets.length;i++){
       entityList.push({value: sheets[i][0], synonyms: sheets[i][1].split(", ")});
@@ -35,9 +35,54 @@ app.intent('擴增知識2', (conv) => {
   conv.json({ 'fulfillmentText': `助教正在審核內容中...` });
 });
 
-app.intent('新增Intents', (conv) => {
-  intentsCreate();
-  conv.json({ 'fulfillmentText': `看一下Intents` });
+app.intent('更新意圖', (conv) => {
+  const SPREADSHEET_ID = '1XFD6g__B-IkfG03wHr7H8QHQWORjZ326MviM3TcsC2Q';
+  return sheetsGet(SPREADSHEET_ID).then((sheets) => {
+    for(let i=1;i<sheets.length;i++){
+      let trainingPhrases = [];
+      let messages = [];
+      let question = sheets[i][1].split(", ");
+      for(let j=0;j<question.length;j++){
+        let keyword = question[j].split("\"");
+        if(keyword.length>1){
+          trainingPhrases.push({
+            "type": "EXAMPLE",
+            "parts": [
+              {
+                "text": keyword[0]
+              },
+              {
+                "text": keyword[1],
+                "alias": "Key",
+                "entityType": "@Keywords",
+                "userDefined": true
+              },
+              {
+                "text": keyword[2]
+              }
+            ]
+          });
+        }
+        else{
+          trainingPhrases.push({
+            "type": "EXAMPLE",
+            "parts": [
+              {
+                "text": keyword[0]
+              }
+            ]
+          });
+        }
+      }
+      messages.push({
+        "text": {
+          "text": [sheets[i][2]]
+        }
+      });
+      intentsUpdate(sheets[i][0], trainingPhrases, messages);
+    }
+    conv.json({ 'fulfillmentText': `看一下Intents` });
+  });
 });
 
 app.intent('選項功能', (conv) => {
