f67f65eca976b9de890c9ff56d1d4a702dca037b
gn01868184 <gn01868184@yahoo.com.tw>
2020-01-30 16:44:39 +0800

    函數需傳入sheetID

--- functions/sheetsGet.js
+++ functions/sheetsGet.js
@@ -1,21 +1,20 @@
 const {google} = require('googleapis');
 const sheets = google.sheets('v4');
 
-const SPREADSHEET_ID = '1UIEQekmLryJJSolpJNc15qar1fp4q-txiQgw1X-leLk';
 const API_KEY = 'AIzaSyDdZyWBklrrnpqzBtbSw6kCr_DVmPVO4To';
 
-module.exports = function () {
+module.exports = function (SPREADSHEET_ID) {
   return new Promise((resolve) => {
     sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       key: API_KEY,
-      range: 'Sheet1',
+      range: '工作表1',
     }, (err, res) => {
       if (err) return console.log('The API returned an error: ' + err);
       const rows = res.data.values;
       if (rows.length) {
         rows.map((row) => {
-          console.log(`${row[0]}, ${row[1]}`);
+          console.log(`${row[0]}, ${row[1]}, ${row[2]}`);
         });
         resolve(rows);
       } else {
