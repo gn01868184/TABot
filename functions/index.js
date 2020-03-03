c2cbbc5db72c02525f0c6937ef6508635139b32e
gn01868184 <gn01868184@yahoo.com.tw>
2020-03-03 17:19:14 +0800

    新增知識地圖、個人化考題功能

--- functions/index.js
+++ functions/index.js
@@ -9,22 +9,103 @@ const entitiesUpdate = require('./entitiesUpdate.js');
 const firebaseRead = require('./firebaseRead.js');
 const firebaseWrite = require('./firebaseWrite.js');
 const neo4jRead = require('./neo4jRead.js');
-
 const app = dialogflow({
   debug: true,
   // REPLACE THE PLACEHOLDER WITH THE CLIENT_ID OF YOUR ACTIONS PROJECT
   clientId: '48279450292-dqn0hajau3qlq98qncipo4g50rbderpj.apps.googleusercontent.com',
 });
 
-app.intent('連接Neo4j', (conv) => {
-  return neo4jRead().then((URL) => {
-    conv.json({ 'fulfillmentText': `投影片網址: ${URL}` });
+app.intent('課程知識地圖2', (conv) => {
+  const CQL = 'MATCH (a:Section{name: $name})-[rel:Item]-(b) RETURN a,b';
+  return neo4jRead(CQL, conv.body.queryResult.parameters.Keywords).then((req) => {
+    let singleRecord = req.records[0];
+    let node = singleRecord.get(0);
+    let node2 = singleRecord.get(1);
+    let facebookMessages =
+    [
+        {
+          "quickReplies": {
+          "title": `你想了解的${node.properties.name}在${node.properties.URL}\n了解相關課程↓`,
+          "quickReplies": [
+            node2.properties.name
+          ]
+        },
+        "platform": "FACEBOOK"
+      },
+      {
+        "text": {
+          "text": [
+            ""
+          ]
+        }
+      }
+    ];
+    conv.json({ 'fulfillmentMessages': facebookMessages });
   });
 });
 
 app.intent('擴增知識2', (conv) => {
   sheetsUpdate(conv.body.queryResult.parameters.any);
   conv.json({ 'fulfillmentText': `助教正在審核內容中...` });
+});
+
+app.intent('個人化考題', (conv) => {
+  const SPREADSHEET_ID = '1Bezl1MdNN2X06O57Prm1GeetZddC5ttwxh20N2PtyV4';
+  const CQL = 'MATCH (a:Student{name: $name})-[rel]-(b) RETURN b';
+  let number = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  return firebaseRead(number).then((ID) => {
+    return neo4jRead(CQL, ID).then((req) => {
+      let reqLength = req.records.length;
+      console.log('常錯的問題有' + reqLength);
+      let singleRecord = req.records[Math.floor(Math.random()*reqLength)];
+      let node = singleRecord.get(0);
+      console.log('題號' + node.properties.name);
+      return sheetsGet(SPREADSHEET_ID).then((sheets) => {
+        //答案Context
+        const session = conv.body.session.split("/");
+        console.log(session[4]);
+        const client = new dialogflow1.ContextsClient();
+        const formattedParent = client.sessionPath('test-pclcyl', session[4]);
+        const context = {
+          name: client.contextPath('test-pclcyl', session[4], sheets[node.properties.name][6]),
+          lifespanCount: 1
+        };
+        const request = {
+          parent: formattedParent,
+          context: context,
+        };
+        client.createContext(request)
+          .then(responses => {
+            const response = responses[0];
+            console.log(response);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        //回覆
+        let resText =
+        [
+            {
+              "quickReplies": {
+              "title": `${sheets[node.properties.name][1]}\nA:${sheets[node.properties.name][2]}\nB:${sheets[node.properties.name][3]}\nC:${sheets[node.properties.name][4]}\nD:${sheets[node.properties.name][5]}`,
+              "quickReplies": [
+                'A','B','C','D'
+              ]
+            },
+            "platform": "FACEBOOK"
+          },
+          {
+            "text": {
+              "text": [
+                ""
+              ]
+            }
+          }
+        ];
+        conv.json({ 'fulfillmentMessages': resText });
+      });
+    });
+  });
 });
 
 app.intent('讀取學號', (conv) => {
@@ -105,29 +186,29 @@ app.intent('更新意圖', (conv) => {
 });
 
 app.intent('選項功能', (conv) => {
-  let input =[
-      {
-        "quickReplies": {
-          "title": "你有什麼問題呢",
-          "quickReplies": [
-            "FAQ",
-            "課程知識地圖",
-            "個人化功能",
-            "真人回覆"
-          ]
-        },
-        "platform": "FACEBOOK"
-      },
-      {
-        "text": {
-          "text": [
-            ""
-          ]
-        }
-      }
-    ]
+  const session = conv.body.session.split("/");
+  console.log(session[4]);
+  const client = new dialogflow1.ContextsClient();
+  const formattedParent = client.sessionPath('test-pclcyl', session[4]);
+  const context = {
+    name: client.contextPath('test-pclcyl', session[4], "A"),
+    lifespanCount: 2
+  };
+  const request = {
+    parent: formattedParent,
+    context: context,
+  };
+  client.createContext(request)
+    .then(responses => {
+      const response = responses[0];
+      console.log(response);
+      // doThingsWith(response)
 
-    conv.json({ 'fulfillmentMessages': input });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  conv.json({ 'fulfillmentText': `完成` });
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
