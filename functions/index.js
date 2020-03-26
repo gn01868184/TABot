cb60fe43aef99dc6c1b0009467382c1249a45f99
gn01868184 <gn01868184@yahoo.com.tw>
2020-03-26 14:49:41 +0800

    登入功能完成

--- functions/index.js
+++ functions/index.js
@@ -16,11 +16,14 @@ const app = dialogflow({
 });
 
 app.intent('課程知識地圖2', (conv) => {
-  const CQL = 'MATCH (a:Section{name: $name})-[rel:Item]-(b) RETURN a,b';
+  const CQL = 'MATCH (a:Java_Section{name: $name})-[rel:Item]-(b)-[rel2:Item]-(c) RETURN a,c';
   return neo4jRead(CQL, conv.body.queryResult.parameters.Keywords).then((req) => {
-    let singleRecord = req.records[0];
+    let reqLength = req.records.length;
+    let singleRecord = req.records[Math.floor(Math.random()*reqLength)];
     let node = singleRecord.get(0);
     let node2 = singleRecord.get(1);
+    console.log('章節:' + node.properties.name);
+    conv.json({ 'fulfillmentText': `你可能需要了解${node.properties.name}在${node.properties.URL}` });
     let facebookMessages =
     [
         {
@@ -49,76 +52,278 @@ app.intent('擴增知識2', (conv) => {
   conv.json({ 'fulfillmentText': `助教正在審核內容中...` });
 });
 
-app.intent('個人化考題', (conv) => {
-  const SPREADSHEET_ID = '1Bezl1MdNN2X06O57Prm1GeetZddC5ttwxh20N2PtyV4';
-  const CQL = 'MATCH (a:Student{name: $name})-[rel]-(b) RETURN b';
-  let number = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
-  return firebaseRead(number).then((ID) => {
-    return neo4jRead(CQL, ID).then((req) => {
-      let reqLength = req.records.length;
-      console.log('常錯的問題有' + reqLength);
-      let singleRecord = req.records[Math.floor(Math.random()*reqLength)];
-      let node = singleRecord.get(0);
-      console.log('題號' + node.properties.name);
+app.intent('查詢成績', (conv) => {
+  const SPREADSHEET_ID = '1zw_Y-SuJMNPn8-yPQBDIXa3Shq5xBa4by_zj0hZetkg';
+  return sheetsGet(SPREADSHEET_ID).then((sheets) => {
+    sheets[0].splice(0,1);
+    let resText =
+    [
+      {
+        "quickReplies": {
+          "title": `你想查詢什麼成績呢`,
+          "quickReplies": sheets[0]
+        },
+        "platform": "FACEBOOK"
+      },
+      {
+        "text": {
+          "text": [
+            ""
+          ]
+        }
+      }
+    ];
+    conv.json({ 'fulfillmentMessages': resText });
+  });
+});
+
+app.intent('查詢成績2', (conv) => {
+  const SPREADSHEET_ID = '1zw_Y-SuJMNPn8-yPQBDIXa3Shq5xBa4by_zj0hZetkg';
+  let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  let input = conv.body.queryResult.queryText;
+  let firebaseSenderIDRef = `/facebookID/${senderID}`;
+  let studentID = 0;
+  let gradeRow = 0;
+  return firebaseRead(firebaseSenderIDRef).then((refValue) => {
+    studentID = refValue["studentID"];
+    let online = refValue["online"];
+    if(online == true){
       return sheetsGet(SPREADSHEET_ID).then((sheets) => {
-        //答案Context
-        const session = conv.body.session.split("/");
-        console.log(session[4]);
-        const client = new dialogflow1.ContextsClient();
-        const formattedParent = client.sessionPath('test-pclcyl', session[4]);
-        const context = {
-          name: client.contextPath('test-pclcyl', session[4], sheets[node.properties.name][6]),
-          lifespanCount: 1
-        };
-        const request = {
-          parent: formattedParent,
-          context: context,
-        };
-        client.createContext(request)
-          .then(responses => {
-            const response = responses[0];
-            console.log(response);
-          })
-          .catch(err => {
-            console.error(err);
-          });
-        //回覆
-        let resText =
-        [
-            {
-              "quickReplies": {
-              "title": `${sheets[node.properties.name][1]}\nA:${sheets[node.properties.name][2]}\nB:${sheets[node.properties.name][3]}\nC:${sheets[node.properties.name][4]}\nD:${sheets[node.properties.name][5]}`,
-              "quickReplies": [
-                'A','B','C','D'
-              ]
-            },
-            "platform": "FACEBOOK"
-          },
-          {
-            "text": {
-              "text": [
-                ""
-              ]
-            }
+        for(let i=1;i<=sheets.length;i++){
+          if(sheets[i][0] == studentID){
+            studentID = i ;
+            break;
           }
-        ];
-        conv.json({ 'fulfillmentMessages': resText });
+        }
+        for(let i=1;i<=sheets[0].length;i++){
+          if(sheets[0][i] == input){
+            gradeRow = i ;
+            break;
+          }
+        }
+        if(gradeRow == 0){
+          conv.json({ 'fulfillmentText': `請稍後再試一次` });
+        }
+        else if (sheets[studentID][gradeRow] == "") {
+          conv.json({ 'fulfillmentText': `成績尚未公布` });
+        }
+        else{
+          conv.json({ 'fulfillmentText': `${sheets[studentID][gradeRow]}` });
+        }
+
       });
+    }
+  });
+});
+
+app.intent('輸入密碼2', (conv) => {
+  const SPREADSHEET_ID = '1zw_Y-SuJMNPn8-yPQBDIXa3Shq5xBa4by_zj0hZetkg';
+  let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  let input = conv.body.queryResult.queryText;
+  let firebaseSenderIDRef = `/facebookID/${senderID}`;
+  let studentID = 0;
+  return firebaseRead(firebaseSenderIDRef).then((refValue) => {
+    let studentID = refValue["studentID"];
+    let online = refValue["online"];
+    let firebaseStudentIDIDRef = `/studentID/${studentID}`;
+    if(online == true){
+      conv.json({ 'fulfillmentText': `你已經登入囉，直接使用功能就可以了` });
+    }
+    else{
+      return firebaseRead(firebaseStudentIDIDRef).then((refValue) => {
+        let password = refValue["password"];
+        if(input == password){
+          firebaseWrite(senderID, studentID, true);
+          conv.json({ 'fulfillmentText': `登入成功` });
+        }
+        else{
+          conv.json({ 'fulfillmentText': `學號或密碼輸入錯誤` });
+        }
+      });
+    }
+  });
+});
+
+app.intent('個人化教材', (conv) => {
+  const CQL = 'MATCH (a:Java109_Student{name: $name})-[rel]-(b)-[rel2]-(c)  RETURN c';
+  let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  let firebaseSenderIDRef = `/facebookID/${senderID}`;
+  return firebaseRead(firebaseSenderIDRef).then((refValue) => {
+    let studentID = refValue["studentID"];
+    return neo4jRead(CQL, studentID).then((req) => {
+      let reqLength = req.records.length;
+      if(reqLength != 0){
+        let singleRecord = req.records[Math.floor(Math.random()*reqLength)];
+        let node = singleRecord.get(0);
+        console.log('章節:' + node.properties.name);
+        conv.json({ 'fulfillmentText': `你可能需要了解${node.properties.name}在${node.properties.URL}` });
+      }
+      else{
+        conv.json({ 'fulfillmentText': `你目前沒有個人化教材唷!` });
+      }
     });
   });
 });
 
+app.intent('個人化考題', (conv) => {
+  const SPREADSHEET_ID = '1Bezl1MdNN2X06O57Prm1GeetZddC5ttwxh20N2PtyV4';
+  const CQL = 'MATCH (a:Java109_Student{name: $name})-[rel]-(b) RETURN b';
+  let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  let firebaseSenderIDRef = `/facebookID/${senderID}`;
+  return firebaseRead(firebaseSenderIDRef).then((refValue) => {
+    let studentID = refValue["studentID"];
+    return neo4jRead(CQL, studentID).then((req) => {
+      let reqLength = req.records.length;
+      console.log('常錯的問題有' + reqLength);
+      if(reqLength != 0){
+        let singleRecord = req.records[Math.floor(Math.random()*reqLength)];
+        let node = singleRecord.get(0);
+        console.log('題號' + node.properties.name);
+        return sheetsGet(SPREADSHEET_ID).then((sheets) => {
+          //答案Context
+          const session = conv.body.session.split("/");
+          console.log(session[4]);
+          const client = new dialogflow1.ContextsClient();
+          const formattedParent = client.sessionPath('test-pclcyl', session[4]);
+          const context = {
+            name: client.contextPath('test-pclcyl', session[4], sheets[node.properties.name][6]),
+            lifespanCount: 1
+          };
+          const request = {
+            parent: formattedParent,
+            context: context,
+          };
+          client.createContext(request)
+            .then(responses => {
+              const response = responses[0];
+              console.log(response);
+            })
+            .catch(err => {
+              console.error(err);
+            });
+          //回覆
+          let resText =
+          [
+              {
+                "quickReplies": {
+                "title": `${sheets[node.properties.name][1]}\nA:${sheets[node.properties.name][2]}\nB:${sheets[node.properties.name][3]}\nC:${sheets[node.properties.name][4]}\nD:${sheets[node.properties.name][5]}`,
+                "quickReplies": [
+                  'A','B','C','D'
+                ]
+              },
+              "platform": "FACEBOOK"
+            },
+            {
+              "text": {
+                "text": [
+                  ""
+                ]
+              }
+            }
+          ];
+          conv.json({ 'fulfillmentMessages': resText });
+        });
+      }
+      else{
+        conv.json({ 'fulfillmentText': `你目前沒有個人化考題唷!` });
+      }
+    });
+  });
+});
+
+app.intent('個人化考題_A', (conv) => {
+  let answer = conv.body.queryResult.queryText;
+  switch (answer) {
+    case 'A':
+      conv.json({ 'fulfillmentText': `答對囉!` });
+      break;
+    case 'B':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是A` });
+      break;
+    case 'C':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是A` });
+      break;
+    case 'D':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是A` });
+      break;
+    default:
+      conv.json({ 'fulfillmentText': `你案太快了~稍後再說一次你要的功能` });
+  }
+});
+
+app.intent('個人化考題_B', (conv) => {
+  let answer = conv.body.queryResult.queryText;
+  switch (answer) {
+    case 'A':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是B` });
+      break;
+    case 'B':
+      conv.json({ 'fulfillmentText': `答對囉!` });
+      break;
+    case 'C':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是B` });
+      break;
+    case 'D':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是B` });
+      break;
+    default:
+      conv.json({ 'fulfillmentText': `你案太快了~稍後再說一次你要的功能` });
+  }
+});
+
+app.intent('個人化考題_C', (conv) => {
+  let answer = conv.body.queryResult.queryText;
+  switch (answer) {
+    case 'A':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是C` });
+      break;
+    case 'B':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是C` });
+      break;
+    case 'C':
+      conv.json({ 'fulfillmentText': `答對囉!` });
+      break;
+    case 'D':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是C` });
+      break;
+    default:
+      conv.json({ 'fulfillmentText': `你案太快了~稍後再說一次你要的功能` });
+  }
+});
+
+app.intent('個人化考題_D', (conv) => {
+  let answer = conv.body.queryResult.queryText;
+  switch (answer) {
+    case 'A':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是D` });
+      break;
+    case 'B':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是D` });
+      break;
+    case 'C':
+      conv.json({ 'fulfillmentText': `答錯囉!答案是D` });
+      break;
+    case 'D':
+      conv.json({ 'fulfillmentText': `答對囉!` });
+      break;
+    default:
+      conv.json({ 'fulfillmentText': `你案太快了~稍後再說一次你要的功能` });
+  }
+});
+
 app.intent('讀取學號', (conv) => {
-  let number = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
-  return firebaseRead(number).then((ID) => {
-    conv.json({ 'fulfillmentText': `你的學號是${ID}` });
+  let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
+  let firebaseSenderIDRef = `/facebookID/${senderID}`;
+  return firebaseRead(firebaseSenderIDRef).then((refValue) => {
+    let studentID = refValue["studentID"];
+    conv.json({ 'fulfillmentText': `你的學號是${studentID}` });
   });
 });
 
 app.intent('設定學號2', (conv) => {
   let senderID = JSON.parse(conv.body.originalDetectIntentRequest.payload.data.sender.id);
   let studentID = JSON.parse(conv.body.queryResult.queryText);
-  firebaseWrite(senderID, studentID);
+  firebaseWrite(senderID, studentID, false);
   conv.json({ 'fulfillmentText': `設定好囉` });
 });
 
@@ -183,32 +388,6 @@ app.intent('更新意圖', (conv) => {
     }
     conv.json({ 'fulfillmentText': `Intents更新完成` });
   });
-});
-
-app.intent('選項功能', (conv) => {
-  const session = conv.body.session.split("/");
-  console.log(session[4]);
-  const client = new dialogflow1.ContextsClient();
-  const formattedParent = client.sessionPath('test-pclcyl', session[4]);
-  const context = {
-    name: client.contextPath('test-pclcyl', session[4], "A"),
-    lifespanCount: 2
-  };
-  const request = {
-    parent: formattedParent,
-    context: context,
-  };
-  client.createContext(request)
-    .then(responses => {
-      const response = responses[0];
-      console.log(response);
-      // doThingsWith(response)
-
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  conv.json({ 'fulfillmentText': `完成` });
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
